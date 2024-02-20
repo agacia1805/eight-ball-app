4712b0c2d8a76df10d08923f2f565ca926df6056
agacia1805 <agacia1805@gmail.com>
2024-02-20 20:49:20 +0000

    (feat) remove boilerplate content and basic elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "./ui/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+     <body className={`${inter.className} flex min-h-screen flex-col`}>
+               <div className='flex flex-grow flex-col justify-center'>
+                 {children}
+               </div>
+               <Footer />
+             </body>
     </html>
   );
 }
