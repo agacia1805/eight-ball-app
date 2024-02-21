fa3d319c044855f26da562c539f58afe5d23d3aa
agacia1805 <agacia1805@gmail.com>
2024-02-21 21:33:14 +0000

    (chore) add key prop

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { fetchRandomizedProphecy } from '@/app/lib/data';
 
 export default async function Home() {
 const prophecies = await fetchRandomizedProphecy();
-  return <main>{prophecies.map((prophecy) => <div>{prophecy.content}</div>)}</main>;
+  return <main>{prophecies.map((prophecy) => <div key={prophecy.id}>{prophecy.content}</div>)}</main>;
 }
