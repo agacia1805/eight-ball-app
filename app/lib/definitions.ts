7dfd383e31fcbab14adda1f55c5d906fabb276c9
agacia1805 <agacia1805@gmail.com>
2024-02-21 21:16:40 +0000

    (feat) add type definitions for database data

---
+++ app/lib/definitions.ts
@@ -0,0 +1,12 @@
+export type RandomizedWord = {
+    description: string;
+    id: string;
+    language: string;
+    word: string;
+};
+
+export type RandomizedProphecy = {
+    content: string;
+    id: string;
+    title: string;
+};
