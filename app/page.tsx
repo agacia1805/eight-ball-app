5fedf3bffdbfc2b22d326e62c5c295ca16b01b77
agacia1805 <agacia1805@gmail.com>
2024-02-21 21:31:51 +0000

    (feat) show prophecies on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
+import { fetchRandomizedProphecy } from '@/app/lib/data';
 
-export default function Home() {
-  return <main></main>;
+export default async function Home() {
+const prophecies = await fetchRandomizedProphecy();
+  return <main>{prophecies.map((prophecy) => <div>{prophecy.content}</div>)}</main>;
 }
