43aabb65460ad84173db7176d06876b65d657b24
agacia1805 <agacia1805@gmail.com>
2024-02-22 20:02:29 +0000

    (chore) return null if data is not available

--- app/ui/prophecy.tsx
+++ app/ui/prophecy.tsx
@@ -5,10 +5,11 @@ export default async function Prophecy() {
   const prophecies = await fetchRandomizedProphecy();
   const randomProphecy = getRandomElement(prophecies);
 
+  if (!randomProphecy) return null;
+
   return (
     <div className='text-gray-50'>
       <p className='semibold text-3xl capitalize'>{randomProphecy.title}</p>
-
       <p className='text-xl'>{randomProphecy.content}</p>
     </div>
   );
--- app/ui/word.tsx
+++ app/ui/word.tsx
@@ -5,13 +5,14 @@ export default async function Word() {
   const words = await fetchRandomizedWord();
   const randomWord = getRandomElement(words);
 
+  if (!randomWord) return null;
+
   return (
     <div className='text-gray-50'>
       <p>
-        <span className='semibold text-3xl capitalize'>{randomWord.word}</span>{' '}
+        <span className='semibold text-3xl capitalize'>{randomWord.word}</span>
         <span className='text-sm italic'>({randomWord.language})</span>
       </p>
-
       <p className='text-xl'>- {randomWord.description}</p>
     </div>
   );
