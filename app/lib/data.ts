cbce6af1b9fefe299f262a7f2350e6d36ece18cc
agacia1805 <agacia1805@gmail.com>
2024-02-22 20:02:44 +0000

    (chore) format fix

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,31 +1,29 @@
 import { sql } from '@vercel/postgres';
-import {
-    RandomizedWord, RandomizedProphecy
-} from './definitions';
+import { RandomizedWord, RandomizedProphecy } from './definitions';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRandomizedWord() {
-    noStore();
+  noStore();
 
-    try {
-        const data = await sql<RandomizedWord>`SELECT * FROM randomizedWords`;
-
-        return data.rows;
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch randomized word data.');
-    }
+  try {
+    const data = await sql<RandomizedWord>
+     `SELECT * FROM randomizedWords`;
+     return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch randomized word data.');
+  }
 }
 
 export async function fetchRandomizedProphecy() {
-    noStore();
+  noStore();
 
-    try {
-        const data = await sql<RandomizedProphecy>`SELECT * FROM randomizedProphecies`;
-
-        return data.rows;
-    } catch (error) {
-        console.error('Database Error:', error);
-        throw new Error('Failed to fetch randomized prophecies data.');
-    }
+  try {
+    const data = await sql<RandomizedProphecy>
+    `SELECT * FROM randomizedProphecies`;
+     return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch randomized prophecies data.');
+  }
 }
