b8a5a6705f26fd252530af8c90a85e30eefc5f50
agacia1805 <agacia1805@gmail.com>
2024-02-22 20:38:38 +0000

    (chore) update word and prophecy components styling

--- app/ui/prophecy.tsx
+++ app/ui/prophecy.tsx
@@ -1,5 +1,10 @@
 import { fetchRandomizedProphecy } from '@/app/lib/data';
 import { getRandomElement } from '@/app/lib/utils';
+import { Roboto_Slab } from 'next/font/google';
+
+const robotoSlab = Roboto_Slab({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+});
 
 export default async function Prophecy() {
   const prophecies = await fetchRandomizedProphecy();
@@ -8,9 +13,13 @@ export default async function Prophecy() {
   if (!randomProphecy) return null;
 
   return (
-    <div className='text-gray-50'>
-      <p className='semibold text-3xl capitalize'>{randomProphecy.title}</p>
-      <p className='text-xl'>{randomProphecy.content}</p>
+    <div className='flex flex-col gap-2 text-gray-50 md:max-w-2xl'>
+      <p
+        className={`${robotoSlab.className} semibold text-center text-3xl capitalize`}
+      >
+        {randomProphecy.title}
+      </p>
+      <p className='text-center text-xl italic'>{randomProphecy.content}</p>
     </div>
   );
 }
--- app/ui/word.tsx
+++ app/ui/word.tsx
@@ -1,5 +1,10 @@
 import { fetchRandomizedWord } from '@/app/lib/data';
 import { getRandomElement } from '@/app/lib/utils';
+import { Roboto_Slab } from 'next/font/google';
+
+const robotoSlab = Roboto_Slab({
+  subsets: ['latin', 'latin-ext', 'vietnamese'],
+});
 
 export default async function Word() {
   const words = await fetchRandomizedWord();
@@ -8,12 +13,16 @@ export default async function Word() {
   if (!randomWord) return null;
 
   return (
-    <div className='text-gray-50'>
-      <p>
-        <span className='semibold text-3xl capitalize'>{randomWord.word}</span>
+    <div className='flex flex-col gap-2 text-gray-50 md:max-w-2xl'>
+      <p className='text-center'>
+        <span
+          className={`${robotoSlab.className} semibold text-4xl capitalize`}
+        >
+          {randomWord.word}
+        </span>{' '}
         <span className='text-sm italic'>({randomWord.language})</span>
       </p>
-      <p className='text-xl'>- {randomWord.description}</p>
+      <p className='text-center text-xl italic'>- {randomWord.description}</p>
     </div>
   );
 }
