b494ab4a5a16bd26e6b3fafcfde475ac3449d0c2
agacia1805 <agacia1805@gmail.com>
2024-02-22 19:54:40 +0000

    (feat) create word and prophecy components

---
+++ app/ui/prophecy.tsx
@@ -0,0 +1,15 @@
+import { fetchRandomizedProphecy } from '@/app/lib/data';
+import { getRandomElement } from '@/app/lib/utils';
+
+export default async function Prophecy() {
+  const prophecies = await fetchRandomizedProphecy();
+  const randomProphecy = getRandomElement(prophecies);
+
+  return (
+    <div className='text-gray-50'>
+      <p className='semibold text-3xl capitalize'>{randomProphecy.title}</p>
+
+      <p className='text-xl'>{randomProphecy.content}</p>
+    </div>
+  );
+}
