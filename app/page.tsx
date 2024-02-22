04d927be82d9a131bc08b57c6994c8d7ab2f14ef
agacia1805 <agacia1805@gmail.com>
2024-02-22 19:55:53 +0000

    (feat) add word and prophecy component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { fetchRandomizedProphecy } from '@/app/lib/data';
+import Prophecy from '@/app/ui/prophecy';
+import Word from '@/app/ui/word';
 
-export default async function Home() {
-const prophecies = await fetchRandomizedProphecy();
-  return <main>{prophecies.map((prophecy) => <div key={prophecy.id}>{prophecy.content}</div>)}</main>;
+export default function Home() {
+  return (
+    <main>
+      <Word />
+      <Prophecy />
+    </main>
+  );
 }
