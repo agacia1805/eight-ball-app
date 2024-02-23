6bdc4f48df96c0fd0ae97c156373f926b7e2e045
agacia1805 <agacia1805@gmail.com>
2024-02-23 20:06:26 +0000

    (feat) add search params to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import Prophecy from '@/app/ui/prophecy';
-import Word from '@/app/ui/word';
+import Ball from '@/app/ui/ball';
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
   return (
-    <main className='p-6'>
-      <Word />
-      <Prophecy />
+    <main className='mx-auto my-0 p-4'>
+      <Ball />
+      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+        Button mode 1
+      </button>
+      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+        Button mode 2
+      </button>
     </main>
   );
 }
