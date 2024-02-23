14da30f3db2737f20bad1d1e85aa0834af2e03b7
agacia1805 <agacia1805@gmail.com>
2024-02-23 20:05:59 +0000

    (chore) update components

--- app/ui/ball.tsx
+++ app/ui/ball.tsx
@@ -1,28 +1,28 @@
-"use client";
+'use client';
 
-import { motion, useAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function Ball() {
   return (
-    <div className="container">
-          <motion.div
-            className="sphere"
-            drag
-            dragConstraints={{ left: -100, right: 100 }}
-             whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 1.1 }}
-              drag="x"
-            animate={{
-              rotateX: [0, 360],
-              rotateY: [0, 360],
-              rotateZ: [0, 360],
-            }}
-            transition={{
-              duration: 5,
-              loop: Infinity,
-              ease: 'linear',
-            }}
-          />
-        </div>
+    <div className='container'>
+      <motion.div
+        className='sphere flex items-center justify-center'
+        drag
+        dragConstraints={{ left: -100, right: 100 }}
+        drag='x'
+        animate={{
+          rotateX: [0, 360],
+          rotateY: [0, 360],
+          rotateZ: [0, 360],
+        }}
+        transition={{
+          duration: 5,
+          loop: Infinity,
+          ease: 'linear',
+        }}
+      >
+        <span>Text</span>
+      </motion.div>
+    </div>
   );
-};
+}
