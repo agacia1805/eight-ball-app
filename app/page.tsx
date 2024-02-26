effc8203fbcc2c99150275c4fc3bd0b16a4019e0
agacia1805 <agacia1805@gmail.com>
2024-02-26 20:55:17 +0000

    (feat) add links for different mode and update main page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
-import Ball from '@/app/ui/ball';
+import Link from 'next/link';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
+export default function Home(s) {
   return (
-    <main className='mx-auto my-0 p-4'>
-      <Ball />
-      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
-        Button mode 1
-      </button>
-      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
-        Button mode 2
-      </button>
+    <main className='mx-auto my-0 flex flex-col gap-16 p-4 md:gap-24'>
+      <div className='ball static-ball flex items-center justify-center'>
+        <span>Choose mode and shake the ball</span>
+      </div>
+      <div className='flex flex-col items-center gap-6 md:flex-row md:justify-center md:gap-16'>
+        <Link
+          href='/word'
+          className='text-md flex h-[48px] w-48 items-center justify-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:text-yellow-800 md:p-2 md:px-3'
+        >
+          Word mode
+        </Link>
+        <Link
+          href='/prophecy'
+          className='text-md flex h-[48px] w-48 items-center justify-center justify-center gap-2 rounded-md bg-gray-50 p-3 font-medium hover:text-yellow-800 md:p-2 md:px-3'
+        >
+          Prophecy mode
+        </Link>
+      </div>
     </main>
   );
 }
