1959d7a72067b596367cf0f661d21e9c2c3e93bc
agacia1805 <agacia1805@gmail.com>
2024-02-28 19:24:42 +0000

    (chore) update type definitions to match data

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,12 +1,10 @@
 export type RandomizedWord = {
   description: string;
-  id: string;
   language: string;
   word: string;
 };
 
 export type RandomizedProphecy = {
   content: string;
-  id: string;
   title: string;
 };
