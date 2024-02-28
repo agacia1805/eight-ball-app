7330c413dc0b453e225d548a669d8eb206e0f324
agacia1805 <agacia1805@gmail.com>
2024-02-28 19:25:20 +0000

    (chore) update error page to match styling

--- app/error.tsx
+++ app/error.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { useEffect } from 'react';
+import { TbError404 } from "react-icons/tb";
 
 export default function Error({ reset }: { reset: () => void }) {
   return (
     <main className='flex h-full flex-col items-center justify-center'>
-      <h2 className='text-center'>Something went wrong!</h2>
+    <TbError404 size={120} className="text-white"/>
+      <h2 className='text-center text-2xl text-white'>Something went wrong!</h2>
       <button
-        className='mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400'
+        className='mt-8 rounded-md bg-gray-300 px-8 py-2 text-base text-black transition-colors hover:text-yellow-800'
         onClick={() => reset()}
       >
         Try again
