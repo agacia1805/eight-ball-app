fb12c52876262c3b665f9d5aec57d4ac93ce17fc
agacia1805 <agacia1805@gmail.com>
2024-02-29 20:54:09 +0000

    (feat) add shake animation for the ball component

--- app/ui/ball.tsx
+++ app/ui/ball.tsx
@@ -12,25 +12,54 @@ export default function Ball({
 }) {
   const [isBallClicked, setIsBallClicked] = useState<boolean>(false);
   const [ballClickCount, setBallClickCount] = useState<number>(0);
+  const [isShaking, setIsShaking] = useState<boolean>(false);
+
+  const shake = () => {
+    setIsShaking(true);
+    setTimeout(() => setIsShaking(false), 700);
+  };
 
   const handleBallClick = () => {
     setIsBallClicked(true);
     setBallClickCount((prevCount) => prevCount + 1);
+    shake();
   };
 
   const childArray = React.Children.toArray(children);
+
+ const earthquakeVariants = {
+    shaking: {
+      x: [-25, 25, -25, 25, 0],
+      y: [45, -45, 45, -45, 0],
+      transition: {
+                          duration: 0.7,
+                          ease: [0, 0.71, 0.2, 1.01],
+                          scale: {
+                            type: "spring",
+                            damping: 5,
+                            stiffness: 100,
+                            restDelta: 0.001
+                          }
+                        },
+    },
+  };
 
   return (
     <div className='container'>
       <motion.div
         className={`${className} ball flex cursor-pointer select-none items-center justify-center`}
-        onClick={handleBallClick}
+        onClick={() => {
+          if (!isShaking) handleBallClick();
+        }}
+             animate={isShaking ? "shaking" : ""}
+                     variants={earthquakeVariants}
       >
         {React.Children.map(childArray, (child: ReactNode) => {
           if (React.isValidElement(child)) {
             return React.cloneElement(child as ReactElement, {
               isBallClicked,
               ballClickCount,
+              isShaking
             });
           }
           return child;
